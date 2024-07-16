8e3430b6eaadd6f1236aa48260847bfd31e311fd
nitesh <niteshballa.devops@gmail.com>
2024-07-16 20:44:03 +0530

    array of fruits

--- array.sh
+++ array.sh
@@ -2,10 +2,10 @@
 
 FRUITS=("Apple" "Banana" "Mango")
 
-echo "First value: ${FRUITS[0]}"
+echo "First value: $FRUITS[0]"
 
-echo "Second value: ${FRUITS[1]}"
+echo "Second value: $FRUITS[1]"
 
-echo "Second value: ${FRUITS[2]}"
+echo "Second value: $FRUITS[2]"
 
-echo "All Fruits: ${FRUITS[@]}"
+echo "All Fruits: $FRUITS[@]"
