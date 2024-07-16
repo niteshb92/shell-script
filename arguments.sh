198de8f43fb571e5486dae2fd578bb43c25c1d9e
nitesh <niteshballa.devops@gmail.com>
2024-07-16 20:28:00 +0530

    conversation between to persons through arg

---
+++ arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1 : Hi $PERSON2, Good Morning"
+echo "$PERSON2 : Hi $PERSON1, Very Good Morning"
+echo "$PERSON1 : How are you $PERSON2?"
+echo "$PERSON2 : I am good $PERSON1! what about you?"
